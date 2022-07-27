d12f7c109c8454b7e2df2bb1eff7979ed220c56c
hardshipgrants <januaryfoundation00@gmail.com>
2022-07-27 12:41:41 +0530

    update build

---
+++ build/_app/immutable/chunks/posts-d667dd35.js
@@ -0,0 +1,1 @@
+import{a,c as s}from"./menuSubitems-620ce8bb.js";a.get(`${s.ENDPOINT_URL}faqs`).then(t=>t.data);const e=a.get(`${s.ENDPOINT_URL}blog-posts`).then(t=>t.data);export{e as p};
--- build/_app/immutable/chunks/posts-da78517f.js
+++
@@ -1,1 +0,0 @@
-import{a,c as s}from"./menuSubitems-d774bbe4.js";a.get(`${s.ENDPOINT_URL}faqs`).then(t=>t.data);const e=a.get(`${s.ENDPOINT_URL}blog-posts`).then(t=>t.data);export{e as p};
