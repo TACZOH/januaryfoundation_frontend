2b71ef1fafc2b41eac865890e803e7dfa901ec57
hardshipgrants <januaryfoundation00@gmail.com>
2022-09-15 21:17:50 +0530

    fix: form issues

--- src/components/RegisterForm.svelte
+++ src/components/RegisterForm.svelte
@@ -9,17 +9,17 @@
 	$: data = {
 		email: '',
 		password: '',
-    fullName: ''
+		fullName: ''
 	};
 	$: error = {
 		msg: ''
 	};
-	$: loading = false
+	$: loading = false;
 	const onSubmit = async () => {
 		if (data.email && data.password && data.fullName) {
 			try {
-				loading = true
-				const { data: apiRes } = await axios.post(`${config.ENDPOINT_URL}/auth/local/register` , {
+				loading = true;
+				const { data: apiRes } = await axios.post(`${config.ENDPOINT_URL}auth/local/register`, {
 					username: data.fullName,
 					email: data.email,
 					password: data.password
@@ -28,12 +28,12 @@
 					localStorage.setItem('jwt', apiRes?.jwt);
 					localStorage.setItem('email', apiRes?.user.email);
 					localStorage.setItem('username', apiRes?.user.username);
-					loading = false
+					loading = false;
 					return (location.href = '/');
 				}
 			} catch (err) {
 				console.log(error);
-				loading = false
+				loading = false;
 				error.msg = 'Please fill correct details.';
 				localStorage.removeItem('jwt');
 				localStorage.removeItem('email');
@@ -68,7 +68,26 @@
 						data-mdb-ripple-color="light"
 						class="inline-block mt-3"
 					>
-						<svg class="inline-block h-8 cursor-pointer" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 48 48"><defs><path id="a" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"/></defs><clipPath id="b"><use xlink:href="#a" overflow="visible"/></clipPath><path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z"/><path clip-path="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z"/><path clip-path="url(#b)" fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z"/><path clip-path="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z"/></svg>
+						<svg
+							class="inline-block h-8 cursor-pointer"
+							xmlns="http://www.w3.org/2000/svg"
+							xmlns:xlink="http://www.w3.org/1999/xlink"
+							viewBox="0 0 48 48"
+							><defs
+								><path
+									id="a"
+									d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
+								/></defs
+							><clipPath id="b"><use xlink:href="#a" overflow="visible" /></clipPath><path
+								clip-path="url(#b)"
+								fill="#FBBC05"
+								d="M0 37V11l17 13z"
+							/><path clip-path="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z" /><path
+								clip-path="url(#b)"
+								fill="#34A853"
+								d="M0 37l30-23 7.9 1L48 0v48H0z"
+							/><path clip-path="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z" /></svg
+						>
 					</button>
 					<script>
 						startApp();
@@ -86,16 +105,13 @@
 				>
 					<p class="text-center font-semibold mx-4 mb-0">Or</p>
 				</div>
-				
+
 				{#if error.msg.length > 0}
-				<div class="text-red-500 font-bold my-2">{error.msg}</div>
+					<div class="text-red-500 font-bold my-2">{error.msg}</div>
 				{/if}
 				<form on:submit|preventDefault={onSubmit}>
-				<div class="mb-6">
-						<FullName
-							bind:value={data.fullName}
-							title="Full name"
-						/>
+					<div class="mb-6">
+						<FullName bind:value={data.fullName} title="Full name" />
 					</div>
 					<div class="mb-6">
 						<Email bind:value={data.email} title="Email address" />
